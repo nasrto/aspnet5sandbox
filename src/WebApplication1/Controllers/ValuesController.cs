fa5aef68500c8989c710936279880537eb0ab699
orantanen <olli.rantanen@evolvit.fi>
2015-05-20 12:36:16 +0300

    added value

--- src/WebApplication1/Controllers/ValuesController.cs
+++ src/WebApplication1/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace WebApplication1.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2", "value3", "value4", "value5" };
+            return new string[] { "value1", "value2", "value3", "value4", "value5", "value6" };
         }
 
         // GET api/values/5
